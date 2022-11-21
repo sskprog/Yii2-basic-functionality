db89b62a3127789a02cb2de8355eeb2482580915
Stas Kushnir <stas.kushnir@gmail.com>
2022-11-21 21:50:57 +0300

    add field e-mail to table users

--- migrations/m221121_153718_create_users_table.php
+++ migrations/m221121_153718_create_users_table.php
@@ -17,6 +17,7 @@ class m221121_153718_create_users_table extends Migration
             'username' => $this->string(50)->unique(),
             'fullname' => $this->string(50),
             'password' => $this->string(50),
+            'email' => $this->string(50),
             'auth_key' => $this->string(100)->unique(),
             'created_at' => $this->dateTime(),
         ], );
