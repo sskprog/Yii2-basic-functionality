f26c72a6f3db48a524d1f597b3e2d375b0b023e3
Stas Kushnir <stas.kushnir@gmail.com>
2022-11-22 18:24:14 +0300

    add link for user in _list

--- views/post/_list.php
+++ views/post/_list.php
@@ -6,11 +6,9 @@ use yii\helpers\StringHelper;
 ?>
  
 <div class="news-item">
-    <h2 class="page-header"><?= Html::encode($model->title) ?></h2>  
-    <p><span class="glyphicon glyphicon-calendar"></span>
-    <em><?=Yii::$app->formatter->asDate($model->created_at, 'php:d.m.Y')?></em>
-    <em><?php echo $model->user->fullname; ?></em></p>
-    
+    <h2 class="page-header"><?= Html::encode($model->title) ?></h2> 
+    <div><?php echo Html::a($model->user->fullname, ['user', 'id' => $model->user_id]); ?></div>    
+    <div><em ><?=Yii::$app->formatter->asDate($model->created_at, 'php:d.m.Y')?></em></div> 
     
     <?= HtmlPurifier::process(StringHelper::truncate($model->body, 250))?> 
     <?php if (mb_strlen($model->body) > 249):
