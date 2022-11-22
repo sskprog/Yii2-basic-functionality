23eec6085a82854deebd69b06c916b69d2fbd965
Stas Kushnir <stas.kushnir@gmail.com>
2022-11-22 17:56:36 +0300

    add action view for PostController

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -2,17 +2,19 @@
 namespace app\controllers;
 
 use app\models\Posts;
+use app\models\User;
+use yii\web\Controller;
 use yii\data\ActiveDataProvider;
+use yii\web\HttpException;
 
-class PostController extends \yii\web\Controller
+class PostController extends Controller
 {
     public function actionIndex()
     {
         $query = Posts::find()
         ->joinWith('user')
         ->where(['is_published' => 1])
-        ->orderBy(['created_at' => SORT_DESC])
-        ->all();
+        ->orderBy(['created_at' => SORT_DESC]);
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
@@ -21,5 +23,35 @@ class PostController extends \yii\web\Controller
             ],
         ]);
         return $this->render('index', ['dataProvider' => $dataProvider]);
+    }
+
+    public function actionView($id)
+    {
+        $model = Posts::findOne(['id' => $id]);
+
+        if ($model) {
+            $author = User::findOne(['id' => $model->user_id]);
+            return $this->render('view', [
+                'model' => $model, 'author' => $author
+            ]);
+        } else {
+            throw new HttpException(404, 'Страница не найдена');
+        }
+    }
+
+    public function actionUser($id)
+    {
+        var_dump($id);
+        die;
+        $model = Posts::findOne(['id' => $id]);
+
+        if ($model) {
+            $author = User::findOne(['id' => $model->user_id]);
+            return $this->render('view', [
+                'model' => $model, 'author' => $author
+            ]);
+        } else {
+            throw new HttpException(404, 'Страница не найдена');
+        }
     }
 }
