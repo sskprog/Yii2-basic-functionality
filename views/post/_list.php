1b93b1a7399db042a7374518902abd406a852d1a
Stas Kushnir <stas.kushnir@gmail.com>
2023-01-07 22:58:12 +0300

    add styles to views/post_list.php

--- views/post/_list.php
+++ views/post/_list.php
@@ -1,19 +1,22 @@
 <?php
+
 use yii\helpers\Html;
 use yii\helpers\HtmlPurifier;
 use yii\helpers\StringHelper;
 
 ?>
- 
-<div class="news-item">
-    <h2 class="page-header"><?= Html::encode($model->title) ?></h2> 
-    <div><?php echo Html::a($model->user->fullname, ['user', 'id' => $model->user_id]); ?></div>    
-    <div><em ><?=Yii::$app->formatter->asDate($model->created_at, 'php:d.m.Y')?></em></div> 
-    
-    <?= HtmlPurifier::process(StringHelper::truncate($model->body, 250))?> 
-    <?php if (mb_strlen($model->body) > 249):
-    echo Html::a('Читать далее', ['view', 'id' => $model->id]);
-     endif?>
+
+<div class="card">
+    <div class="card-body">
+        <h2 class="card-title"><?= Html::encode($model->title) ?></h2>
+        <div class="card-text"><?php echo Html::a($model->user->fullname, ['user', 'id' => $model->user_id]); ?>
+            <span class="float-end"><?= Yii::$app->formatter->asDate($model->created_at, 'php:d.m.Y') ?></span>
+        </div>
+        <div class="card-text"><?= HtmlPurifier::process(StringHelper::truncate($model->body, 250)) ?></div>
+            <?php if (mb_strlen($model->body) > 249) :
+                echo
+                Html::a('Читать далее', ['view', 'id' => $model->id], ['class' => 'btn btn-primary btn-sm float-end']);
+            endif ?>
+    </div>
 </div>
-
-
+<p></p>
