275883bb24e742833ce1e1c1549738ab82fb2269
Stas Kushnir <stas.kushnir@gmail.com>
2023-01-08 12:50:25 +0300

    setup pager

--- views/post/index.php
+++ views/post/index.php
@@ -7,4 +7,11 @@ echo ListView::widget([
     'dataProvider' => $dataProvider,
     'itemView' => '_list',
     'layout' => "{items}\n{pager}",
+    'pager' => [
+        'prevPageLabel' => 'Назад',
+        'prevPageCssClass' => 'page-item',
+        'nextPageLabel' => 'Вперед',
+        'linkOptions' => ['class' => 'page-link'],
+        'disabledListItemSubTagOptions' => ['class' => 'page-link'],
+    ],
 ]);
